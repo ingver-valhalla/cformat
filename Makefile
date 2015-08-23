1db7559c4bc5ba14682025d04bf0dc414e30b28d
Igor Schelkonogov <ingver.yum@gmail.com>
2015-08-23 22:06:24 +0500

    Fixed same_file()

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ SOURCE = in_out.c cformat.c
 cformat: $(OBJ)
 	gcc -o cformat $(OBJ)
 in_out.o : in_out.c
-	gcc -c in_out.c
+	gcc -g -c in_out.c
 cformat.o : cformat.c 
-	gcc -c cformat.c
+	gcc -g -c cformat.c
 clean:
 	rm cformat *.o
--- in_out.c
+++ in_out.c
@@ -10,39 +10,43 @@
 
 int get_line( char *line, int lim )
 {
-    int c;
-    char *p = line;
-    while( p < line + lim-1 && (c = getchar()) != EOF && c != '\n' ) {
-        *p++ = c;
-    }
-    *p = '\0';
-    return p - line;
+	int c;
+	char *p = line;
+	while( p < line + lim-1 && (c = getchar()) != EOF && c != '\n' ) {
+		*p++ = c;
+	}
+	*p = '\0';
+	return p - line;
 }
 
 bool open_file( FILE **fp, char *file_name, char *mode )
 {
-    if( fp == NULL ) {
-        return false;
-    }
-    *fp = fopen( file_name, mode );
-    if( *fp == NULL ) {
-        fprintf( stderr, "Cannot open file: %s\n", file_name );
-        return false;
-    }
-    return true;
+	if( fp == NULL ) {
+		return false;
+	}
+	*fp = fopen( file_name, mode );
+	if( *fp == NULL ) {
+		fprintf( stderr, "Cannot open file: %s\n", file_name );
+		return false;
+	}
+	return true;
 }
 
 bool same_file( char * fname1, char * fname2 )
 {
 	char buf1[PATH_MAX];
 	char buf2[PATH_MAX];
-	char * real1;
-	char * real2;
-	
-	real1 = realpath( fname1, buf1 );
-	real2 = realpath( fname2, buf2 );
 
-	if( !strcmp( real1, real2 ) ) {
+	if( !realpath( fname1, buf1 ) ) {
+		fprintf( stderr, "Can't find file: %s\n", fname1 );
+		exit( EXIT_FAILURE );
+	}
+	if( !realpath( fname2, buf2 ) ) {
+		fprintf( stderr, "Can't fine file: %s\n", fname2 );
+		exit( EXIT_FAILURE );
+	}
+
+	if( !strcmp( buf1, buf2 ) ) {
 		return true;
 	}
 	return false;
