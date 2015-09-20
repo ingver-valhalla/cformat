b794c2a70781a7fccf7e8ead0238b58da534de6c
Igor Schelkonogov <ingver.yum@gmail.com>
2015-09-20 19:48:00 +0500

    Added test for parser

--- Makefile
+++ Makefile
@@ -1,12 +1,19 @@
 OBJ = in_out.o buf.o token.o
 SOURCE = in_out.c cformat.c token.c
 
-cformat: $(OBJ) cformat.o 
+cformat: $(OBJ) parser.o cformat.o 
 	gcc -g -o cformat $(OBJ) cformat.o
+	
 test_token: $(OBJ) test_token.o
-	gcc -g -o test_token $(OBJ) test_token.o
+	gcc -o test_token $(OBJ) test_token.o
 test_token.o: test_token.c
 	gcc -g -Wall -c test_token.c
+
+test_parser: $(OBJ) parser.o test_parser.o
+	gcc -o test_parser $(OBJ) parser.o test_parser.o
+test_parser.o: test_parser.c
+	gcc -g -Wall -c test_parser.c
+
 in_out.o: in_out.c in_out.h 
 	gcc -g -Wall -c in_out.c
 buf.o: buf.c buf.h
@@ -17,5 +24,6 @@ parser.o: parser.c parser.h
 	gcc -g -Wall -c parser.c
 cformat.o: cformat.c 
 	gcc -g -Wall -c cformat.c
+
 clean:
 	rm cformat test_token *.o
---
+++ test_parser.c
@@ -0,0 +1,160 @@
+// test_parser.c
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "in_out.h"
+#include "token.h"
+#include "parser.h"
+
+
+const char * str_check = "int " "\'sjkdfhjkh\'  ""\"string\" "" kjhsdf ""\"STROKA\"";
+const char * token_str[] = 
+{
+    "NOTOKEN",
+
+    "PREPROC",
+    "MUL_COMMENT",
+    "COMMENT",
+
+    "IF_KW",
+    "ELSE_KW",
+    "FOR_KW",
+    "WHILE_KW",
+    "DO_KW",
+    "SWITCH_KW",
+    "CASE_KW",
+    "DEFAULT_KW",
+
+    "IDENT",
+    "NUM_CONST",
+    "CHR_LIT",
+    "STR_LIT",
+
+    "LPAREN",
+    "RPAREN",
+    "LBRACE",
+    "RBRACE",
+    "LBRACKET",
+    "RBRACKET",
+
+    "ASSIGN_OP",
+    "OP",
+    "ELLIPSIS",
+    "STRUCT_SEP",
+
+    "COMMA",
+    "SEMICOLON",
+    "COLON",
+    "QUESTION",
+
+    "EOL_TOK",
+    "EOF_TOK",
+
+    "LAST_TOKEN_TYPE"
+};
+
+
+void fail( char * msg )
+{
+	puts( msg );
+	exit( EXIT_FAILURE );
+}
+
+int alloc_buffer( char * fname, FileBuf * buf )
+{
+	struct stat fstat;
+	if( stat( fname, & fstat ) ) {
+		printf( "Unable to get stat of %s\n", fname );
+		return 0;
+	}
+	if( !alloc_fbuf( buf, fstat.st_size ) ) {
+		return 0;
+	}
+	return 1;
+}
+
+int how_much_eols( Token * tk )
+{
+	char *p = tk->start;
+	int eols = 0;
+	while( p != tk->end ) {
+		if( *p == '\n' ) 
+			++eols;
+		++p;
+	}
+	return eols;
+}
+
+void print_token( Token * tk, int cur_line, FILE * out )
+{
+	printf( "Line: %3d @ Type: %-11s @ ", cur_line, token_str[tk->type] ); 
+	const char * p = tk->start;
+	while( p != tk->end ) {
+		if( *p == ' ' )
+			putchar('@');
+		else 
+			putchar( *p );
+		++p;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	FILE * in;
+	FILE * out;
+	char * in_fname = "example.c";
+	char * out_fname = "out.txt";
+	if( !open_file( &in, in_fname, "r" ) ) {
+		fail( "Couldn't open input" );
+	}
+	puts( "Opened input" );
+	FileBuf buf = new_fbuf();
+	if( !alloc_buffer( in_fname, &buf) ) {
+		fail( "Couldn't alloc buffer" );
+	}
+	puts( "Allocated buffer" );
+	if( !read_to_buf( in, &buf ) ) {
+		fail( "Couldn't read to buffer" );
+	}
+
+	
+	if( !open_file( &out, out_fname, "w" ) ) {
+		fail( "Couldn't open output" );
+	}
+
+	Parser parser = new_parser();
+	parser.buf = &buf;
+	puts( "Created parser" );
+
+	puts( "Parsing..." );
+	parse( &parser, out );
+	/*while( 1 ) {*/
+		/*tk = get_token( buf.head );*/
+		/*if( tk.type == NOTOKEN ) {*/
+			/*printf( "Got no token\n" );*/
+			/*break;*/
+		/*}*/
+		/*buf.head = tk.end;*/
+		/*if( tk.type != EOL_TOK )*/
+			/*print_token( &tk, cur_line, stdout );*/
+
+		/*if( tk.type == EOL_TOK ) {*/
+			/*++cur_line;*/
+		/*}*/
+		/*if( tk.type == PREPROC*/
+		    /*|| tk.type == MUL_COMMENT*/
+		    /*|| tk.type == STR_LIT )*/
+		/*{*/
+			/*cur_line += how_much_eols( &tk );*/
+		/*}*/
+	/*}*/
+
+	close_file( &in );
+	close_file( &out );
+	
+	return 0;
+}
